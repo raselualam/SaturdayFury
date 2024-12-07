4db22924b4896486ff62a201ce0038aa29c293e4
raselualam <raselualam@gmail.com>
2024-12-07 17:55:04 -0500

    test

--- src/com/upskill/java_1/AccessModifier.java
+++ src/com/upskill/java_1/AccessModifier.java
@@ -4,7 +4,7 @@ public class AccessModifier {
 	
 	public String name = "Rasel";  	// public - Any class can access 
 	
-	private int age = 28; 			// private - Only same class can access
+	private int age = 29; 			// private - Only same class can access
 	
 	protected int ssn = 1245678935; // protected - classes in the same package and subclass
 
